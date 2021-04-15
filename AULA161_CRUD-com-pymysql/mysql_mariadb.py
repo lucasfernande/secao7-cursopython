ac0b73bad975306b710d7d63ba5d79749dc9e2c1
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-15 16:16:20 -0300

    inserindo vários registro de uma vez

--- AULA161_CRUD-com-pymysql/mysql_mariadb.py
+++ AULA161_CRUD-com-pymysql/mysql_mariadb.py
@@ -20,6 +20,25 @@ def conectar():
         conn.close()
 
 
+# with conectar() as conn:
+#     with conn.cursor() as cursor:
+#         query = 'INSERT INTO clientes(nome, sobrenome, idade, peso) VALUES(%s, %s, %s, %s)'
+#         cursor.execute(query, ('Jack', 'Bauer', 64, 70))
+#         conn.commit()
+
+with conectar() as conn:
+    with conn.cursor() as cursor:
+        query = 'INSERT INTO clientes(nome, sobrenome, idade, peso) VALUES(%s, %s, %s, %s)'
+
+        data = [
+            ('Mariana', 'Figueiredo', 26, 55),
+            ('Pedro', 'Henrique', 21, 68),
+            ('Rose', 'Park', 25, 53)
+        ]
+
+        cursor.executemany(query, data)
+        conn.commit()
+
 with conectar() as conn:  # esse with fecha a conexão automaticamente
     with conn.cursor() as cursor:  # esse with fecha o cursor automaticamente
         cursor.execute('SELECT * FROM clientes')
